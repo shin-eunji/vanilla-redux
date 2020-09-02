a113a455267fb943de0c1f21443e125a858f9a39
shineunji <dmswlckato@gmail.com>
2020-09-02 14:03:50 +0900

    dispatch type, text

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
+import {createStore} from "redux";
+
 const form = document.querySelector("form")
 const input = document.querySelector("input")
 const ul = document.querySelector("ul")
 
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
 
-const createTodo = todo => {
-    const li = document.createElement('li')
-    li.innerText = todo
-    ul.appendChild(li)
+
+const reducer = (state = [], action) => {
+    console.log("action", action);
+    switch (action.type) {
+        case ADD_TODO:
+            return [];
+        case DELETE_TODO:
+            return [];
+        default:
+            return state
+    }
 }
+
+const store = createStore(reducer)
 
 const onSubmit = e => {
     e.preventDefault();
     const todo = input.value;
-    input.value = ''
-    createTodo(todo)
+    input.value = '';
+    store.dispatch({type: ADD_TODO, text: todo})
+
 }
 
 form.addEventListener('submit', onSubmit)
