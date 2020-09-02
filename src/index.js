54cfa0c3b5b3db637f14ee25c1dbf248db34ea87
shineunji <dmswlckato@gmail.com>
2020-09-02 13:30:46 +0900

    Todo

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
-import {createStore} from "redux";
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
 
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span')
-
-number.innerText = 0;
-
-const ADD = "ADD"
-const MINUS = "MINUS"
-
-
-const reducer = (number = 0, action) => {
-    switch (action.type) {
-        case ADD:
-            return number + 1
-
-        case MINUS:
-            return number - 1
-
-        default:
-            return number
-    }
+const createTodo = todo => {
+    const li = document.createElement('li')
+    li.innerText = todo
+    ul.appendChild(li)
 }
 
-
-
-const countStore = createStore(reducer)
-
-const onChnage = () => {
-    number.innerText = countStore.getState()
-}
-countStore.subscribe(onChnage);
-
-
-const handleAdd = () => {
-    countStore.dispatch({type: ADD})
-}
-const handleMinus = () => {
-    countStore.dispatch({type: MINUS})
+const onSubmit = e => {
+    e.preventDefault();
+    const todo = input.value;
+    input.value = ''
+    createTodo(todo)
 }
 
-
-
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+form.addEventListener('submit', onSubmit)
